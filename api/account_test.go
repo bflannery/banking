bac2e12bd579b0690e5d13bf3c07718385626019
Brian Flannery <bflannery66@gmail.com>
2022-07-07 06:35:52 -0500

    implements user login with paseto token

--- api/account_test.go
+++ api/account_test.go
@@ -91,7 +91,7 @@ func TestGetAccountAPI(t *testing.T) {
 			// build stubs
 
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
